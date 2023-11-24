425a1eeafb76ab07b02b4fe3e41ff2c56507520d
Sudarshan Kundalik Satkar <137045881+Sudarshansatkar@users.noreply.github.com>
2023-11-24 04:02:05 -0800

    Add files via upload

---
+++ Data Structures(DSA)/Data-Structure-Notes/DSA-Practice/4)24-11-2023/MaxHeapSort/HeapSort.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+public class HeapSort {
+	static int cnt = 0;
+
+	public static void main(String[] args) {
+		int[] arr = { 65, 35, 26, 13, 23, 12, 91, 51, 20, 75, 19 };
+
+		System.out.println("Given array :");
+		System.out.println(Arrays.toString(arr));
+		heapSort(arr);
+		System.out.println("sorted Array : ");
+		System.out.println(Arrays.toString(arr));
+
+		System.out.println("No. of passes:" + (arr.length - 1));
+	}
+
+	private static void heapSort(int[] arr) {
+
+		int n = arr.length;
+		// convert the array into max heap
+		for (int i = n / 2 - 1; i >= 0; i--) {
+			heapify(arr, n, i);
+		}
+		System.out.println("ss"+Arrays.toString(arr));
+
+		for (int i = n - 1; i >= 0; i--) {
+			int temp = arr[0];
+			arr[0] = arr[i];
+			arr[i] = temp;
+			heapify(arr, i, 0);
+			System.out.println("size:" + i);
+			System.out.println(Arrays.toString(arr));
+		}
+
+	}
+
+	// arr is array, n number of elements in the array,
+	// index of root of the tree the one which we want to heapify
+	private static void heapify(int[] arr, int n, int i) {
+		cnt++;
+		int largest = i;
+		int l = 2 * i + 1;
+		int r = 2 * i + 2;
+		if (l < n && arr[l] > arr[largest]) {
+			largest = l;
+		}
+		if (r < n && arr[r] > arr[largest]) {
+			largest = r;
+		}
+		if (largest != i) {
+			int temp = arr[largest];
+			arr[largest] = arr[i];
+			arr[i] = temp;
+			heapify(arr, n, largest); // to check largest node has child or not then again heapify
+		}
+
+	}
+
+}
---
+++ Data Structures(DSA)/Data-Structure-Notes/DSA-Practice/4)24-11-2023/MinHeapSort/src/com/demo/test/minHeapSort.java
@@ -0,0 +1,59 @@
+package com.demo.test;
+
+import java.util.Arrays;
+
+public class minHeapSort {
+	 	public static void main(String[] args) {
+	 		int[] arr= {65,35,26,13,23,12,91};
+	 	
+	 		System.out.println("Given array :");
+	 		System.out.println(Arrays.toString(arr));
+	 		heapSort(arr);
+	 		System.out.println("sorted Array : ");
+	 		System.out.println(Arrays.toString(arr));
+
+	 	}
+
+	 	private static void heapSort(int[] arr) {
+	 		int n=arr.length;
+	 		//convert the array into max heap
+	 		for(int i= n/2-1;i>=0;i--) {
+	 			heapify(arr,n,i);
+	 		}
+	 		System.out.println(Arrays.toString(arr));
+	 		
+	 		for(int i=n-1;i>=0;i--) {
+	 		int temp=arr[0];
+	 		arr[0]=arr[i];
+	 		 arr[i]=temp;
+	 		 heapify(arr,i,0);
+	 		 System.out.println("size:"+i);
+	 		 System.out.println(Arrays.toString(arr));
+	 		}
+	 		
+	 	}
+
+	 	//arr is array, n number of elements in the array, 
+	 	//index of root of the tree the one which we want to heapify
+	 	private static void heapify(int[] arr, int n, int i) {
+	 		int lowest=i;
+	 		int l=2*i+1;
+	 		int r=2*i+2;
+	 		if(l<n && arr[l]<arr[lowest] ) {
+	 			lowest=l;
+	 		}
+	 		if(r<n && arr[r] <arr[lowest]) {
+	 			lowest=r;
+	 		}
+	 		if(lowest!=i) {
+	 			int temp=arr[lowest];
+	 			arr[lowest]=arr[i];
+	 			arr[i]=temp;
+	 			heapify(arr,n,lowest);
+	 		}
+	 		
+	 	}
+
+	 }
+
+
